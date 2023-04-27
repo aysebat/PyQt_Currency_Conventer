a7f78c40241b46c269bcf6d71c0566a161d1ecca
aysebat <aaysebatt@gmail.com>
2023-04-27 07:01:00 +0300

    add get currency function and call it in the show_cuurency

--- sentence_maker.py
+++ sentence_maker.py
@@ -1,10 +1,23 @@
 from PyQt6.QtWidgets import QApplication, QWidget, QVBoxLayout
 from PyQt6.QtWidgets import QLabel, QPushButton, QLineEdit
+from bs4 import BeautifulSoup
+import requests
 
-def make_sentence():
+def get_currency(in_currency, out_currency):
+  url = f"https://www.x-rates.com/calculator/?from={in_currency}&to={out_currency}&amount=1"
+  content = requests.get(url).text
+  soup = BeautifulSoup(content, 'html.parser')
+  currency = soup.find("span", class_="ccOutputRslt").get_text()
+  get_value= float(currency.split(" ")[0])
+  return get_value
+
+
+def show_currency():
     #this takes the input widget text
-    input_text = text.text()
-    output_label.setText(input_text.capitalize() + '.')
+    input_text = float(text.text())
+    rate = get_currency("USD", "EUR")
+    output = round(input_text * rate, 3)
+    output_label.setText(str(output))
 
 #CRETAE THE APPLICATION INSTANCE
 app = QApplication([])
@@ -19,12 +32,12 @@ text = QLineEdit()
 layout.addWidget(text) #add text to the layout
 
 #add button
-btn = QPushButton("Make Sentence")
+btn = QPushButton("Convert")
 layout.addWidget(btn) #add button to the layout
 #clicked is the signal (the signal could be clicked or checked etc.)
 #when the button is clicked inside the parenteses (slot) will be triggered
 #thw slot is function we need to creaate
-btn.clicked.connect(make_sentence)
+btn.clicked.connect(show_currency)
 
 #create the output that make_sentence function gives
 output_label = QLabel('')
